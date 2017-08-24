e508394adfe9058852623bf41772fe0ca593a9ac
Johan Larsson <johan@designcubical.se>
2017-08-24 21:56:55 +0200

    WIP

--- src/app/config.js
+++ src/app/config.js
@@ -3,11 +3,11 @@ module.exports = {
   // Change this to your firebase configuration! (Add Firebase to your web app)
     FIREBASE_CONFIG: {
 
-        apiKey: 'AIzaSyBW6liV-_v8zoGbXzRaKtJ65MJzTEx50BQ',
-        authDomain: 'beeuchugee.firebaseapp.com',
-        databaseURL: 'https://beeuchugee.firebaseio.com',
-        projectId: 'beeuchugee',
+        apiKey: 'ssssssss',
+        authDomain: 'sssssssss',
+        databaseURL: 'ssssssss',
+        projectId: 'ssssss',
         storageBucket: '',
-        messagingSenderId: '620419621456',
+        messagingSenderId: 'sssssss',
     },
 };
